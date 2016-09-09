45b90f333174f1fd1d86816f9af70274aec771a6
shse14 <te14.sergii.shuklietsov@umea.nti.se>
2016-09-09 09:52:46 +0200

    9:51 Sergey: Skrev regler, variabler och GUI

--- src/Three_in_line.java
+++ src/Three_in_line.java
@@ -5,22 +5,30 @@ public class Three_in_line {
 
     public static void main (String [] args) {
 
-        int 11 = 0;
-        int 12 = 0;
-        int 13 = 0;
-        int 21 = 0;
-        int 22 = 0;
-        int 23 = 0;
-        int 31 = 0;
-        int 32 = 0;
-        int 33 = 0;
+        int pos11 = 0;
+        int pos12 = 0;
+        int pos13 = 0;
+        int pos21 = 0;
+        int pos22 = 0;
+        int pos23 = 0;
+        int pos31 = 0;
+        int pos32 = 0;
+        int pos33 = 0;
 
         System.out.println("Three in line: The game");
         System.out.println(" ");
+        System.out.println("How to play: Select your square by writing ");
+        System.out.println("two numbers in line (ex. 21). First number is");
+        System.out.println("latitude second longitude. ");
+        System.out.println(" ");
+
+
         System.out.println("    1\"2\"3\"");
         System.out.println("1' |_|_|_|");
         System.out.println("2' |_|_|_|");
         System.out.println("3' |_|_|_|");
+
+
 
     }
 }

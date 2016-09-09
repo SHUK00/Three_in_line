1a7960b2f6271c1749386831dac45651bf99e140
shse14 <te14.sergii.shuklietsov@umea.nti.se>
2016-09-09 11:34:13 +0200

    11:30 Sergey: Allt i spelet fungerar förutom vinnare bedömning

--- src/Three_in_line.java
+++ src/Three_in_line.java
@@ -17,6 +17,9 @@ public class Three_in_line {
     static String pos32 = "_";
     static String pos33 = "_";
 
+    static int la = 0;
+    static int lo = 0;
+
     public static void main (String [] args) {
 
 
@@ -27,29 +30,30 @@ public class Three_in_line {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Player (x) turn: ");
-        System.out.print("lat:  ");
-        int la = sc.nextInt();
-        System.out.print("long: ");
-        int lo = sc.nextInt();
+        while (running()) {
+            System.out.println("Player (x) turn: ");
+            System.out.print("lat:  ");
+            la = sc.nextInt();
+            System.out.print("long: ");
+            lo = sc.nextInt();
+            gameX();
 
-        if (la == 1 && lo == 1){
-            pos11 = "x";
+            printGame();
+
+            System.out.println("Player (o) turn: ");
+            System.out.print("lat:  ");
+            la = sc.nextInt();
+            System.out.print("long: ");
+            lo = sc.nextInt();
+            gameO();
+
             printGame();
         }
 
-        System.out.println("Player (o) turn: ");
-        System.out.print("lat:  ");
-        int la = sc.nextInt();
-        System.out.print("long: ");
-        int lo = sc.nextInt();
+    }
 
-        if (la == 1 && lo == 1){
-            pos11 = "o";
-            printGame();
-        }
-
-
+    public static boolean running() {
+        return true;
     }
 
     public static void printGame() {
@@ -58,5 +62,73 @@ public class Three_in_line {
         System.out.println("2' |"+pos21+"|"+pos22+"|"+pos23+"|");
         System.out.println("3' |"+pos31+"|"+pos32+"|"+pos33+"|");
 
+    }
+
+    public static void gameX() {
+        if (la == 1 && lo == 1 && pos11 == "_") {
+            pos11 = "x";
+        }
+        if (la == 1 && lo == 2 && pos12 == "_") {
+            pos12 = "x";
+        }
+        if (la == 1 && lo == 3 && pos13 == "_") {
+            pos13 = "x";
+        }
+        if (la == 2 && lo == 1 && pos21 == "_") {
+            pos21 = "x";
+        }
+        if (la == 2 && lo == 2 && pos22 == "_") {
+            pos22 = "x";
+        }
+        if (la == 2 && lo == 3 && pos23 == "_") {
+            pos23 = "x";
+        }
+        if (la == 3 && lo == 1 && pos31 == "_") {
+            pos31 = "x";
+        }
+        if (la == 3 && lo == 2 && pos32 == "_") {
+            pos32 = "x";
+        }
+        if (la == 3 && lo == 3 && pos33 == "_") {
+            pos33 = "x";
+        }
+        else {
+            System.out.println("False value!");
+        }
+    }
+
+    public static void gameO() {
+        if (la == 1 && lo == 1 && pos11 == "_") {
+            pos11 = "o";
+        }
+        if (la == 1 && lo == 2 && pos12 == "_") {
+            pos12 = "o";
+        }
+        if (la == 1 && lo == 3 && pos13 == "_") {
+            pos13 = "o";
+        }
+        if (la == 2 && lo == 1 && pos21 == "_") {
+            pos21 = "o";
+        }
+        if (la == 2 && lo == 2 && pos22 == "_") {
+            pos22 = "o";
+        }
+        if (la == 2 && lo == 3 && pos23 == "_") {
+            pos23 = "o";
+        }
+        if (la == 3 && lo == 1 && pos31 == "_") {
+            pos31 = "o";
+        }
+        if (la == 3 && lo == 2 && pos32 == "_") {
+            pos32 = "o";
+        }
+        if (la == 3 && lo == 3 && pos33 == "_") {
+            pos33 = "o";
+        }
+        else {
+            System.out.println("False value!");
+
+
+        }
     }
 }

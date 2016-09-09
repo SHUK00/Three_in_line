a413405bf5ef05b2156f4ce8e590a77cec705981
shse14 <te14.sergii.shuklietsov@umea.nti.se>
2016-09-09 09:37:05 +0200

    9:14 Sergey: Har planerat hur spellet kommer att se ut #2

--- src/Three_in_line.java
+++ src/Three_in_line.java
@@ -4,6 +4,23 @@
 public class Three_in_line {
 
     public static void main (String [] args) {
-        System.out.println();
+
+        int 11 = 0;
+        int 12 = 0;
+        int 13 = 0;
+        int 21 = 0;
+        int 22 = 0;
+        int 23 = 0;
+        int 31 = 0;
+        int 32 = 0;
+        int 33 = 0;
+
+        System.out.println("Three in line: The game");
+        System.out.println(" ");
+        System.out.println("    1\"2\"3\"");
+        System.out.println("1' |_|_|_|");
+        System.out.println("2' |_|_|_|");
+        System.out.println("3' |_|_|_|");
+
     }
 }
